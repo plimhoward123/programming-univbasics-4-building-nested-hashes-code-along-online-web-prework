c63924b31add86dc2fc0bdd3d1082cc94f7468e4
Plimon Howard V <plimon.howard@gmail.com>
2020-08-26 20:38:33 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,6 +23,7 @@ def bonus
 
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
+  puts epic_tragedy[:montague][:hero][status]
   puts epic_tragedy
   #Don't touch the following line! The `bonus` method must return our newly modified epic tra
 end
